3ce7f9a993d40cf96a8aaa0eb912eb659794a067
nabalmes <neilbalmes4200@gmail.com>
2024-06-19 18:05:09 +0800

    backtolocal

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'https://net-be.vercel.app',
+  apiUrl: 'https://localhost:5001',
   production: false,
   hmr: false,
   apiVersion: "1"
